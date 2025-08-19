e55fc08af5be837ab4d644925ab6474774e709af
Vitor Cerutti <vitor.lorenzo49@gmail.com>
2025-08-19 16:47:27 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ export const Header: React.FC<HeaderProps> = ({
               <img 
                 src="/metalyics-logo.svg" 
                 alt="Metalyics" 
-                className="h-15 w-auto"
+                className="h-10 w-auto"
               />
             </div>
           </div>
