866385ecbeb2a0535974b05547bcc7c76551f6e9
Vitor Cerutti <vitor.lorenzo49@gmail.com>
2025-08-19 16:46:30 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,8 +48,8 @@ export const Header: React.FC<HeaderProps> = ({
             <div className="flex items-center space-x-2">
               <img 
                 src="/metalyics-logo.svg" 
-                alt="MetaLytics" 
-                className="h-10 w-auto"
+                alt="Metalyics" 
+                className="h-30 w-auto"
               />
             </div>
           </div>
