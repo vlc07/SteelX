7fdb3223a1c260eb071df0ead07e93a64af6a47d
Vitor Cerutti <vitor.lorenzo49@gmail.com>
2025-08-21 22:52:14 -0300

    Updated Presentation.tsx

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -27,10 +27,12 @@ export const Presentation: React.FC<PresentationProps> = ({ t, isDark }) => {
         <div className="text-center">
           <div className="flex justify-center mb-8">
             <img 
-              src="/metalyics-logo.svg" 
-              alt="MetaLytics" 
-              className="h-20 w-auto"
-            />
+             src="/metalyics-logo.svg" 
+             alt="MetaLytics" 
+             className="mx-auto"
+             style={{ height: "150px", width: "auto" }}
+             />
+
           </div>
           <h1 className="text-4xl font-bold mb-4">
             {t('systemOverview')}
